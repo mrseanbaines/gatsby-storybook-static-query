617b105469ee6b81b00acd36523714c27319e09f
Sean Baines <mrseanbaines@gmail.com>
2020-08-13 19:19:37 +0100

    Remove public and cache dirs

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-templates-index-tsx": () => import("./../../src/templates/index.tsx" /* webpackChunkName: "component---src-templates-index-tsx" */),
-  "component---src-templates-page-2-tsx": () => import("./../../src/templates/page-2.tsx" /* webpackChunkName: "component---src-templates-page-2-tsx" */)
-}
-
