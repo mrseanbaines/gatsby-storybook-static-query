fcb96b9289387ae3f837c0b829dd3c9e5997bfd6
Sean Baines <mrseanbaines@gmail.com>
2020-08-13 19:24:13 +0100

    Add stories

---
+++ src/components/footer/footer.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Footer from './footer'
+
+const stories = storiesOf('Footer', module)
+
+stories.add('Default', () => <Footer />)
---
+++ src/components/header/header.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Header from './header'
+
+const stories = storiesOf('Header', module)
+
+// stories.add('Default', () => <Header />)
